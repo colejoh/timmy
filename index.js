91968e2a5551f60eb9479df230e51f8936960565
Cole Johnson <chj@purdue.edu>
2019-06-19 17:22:29 -0400

    Switch to handlebars

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express         = require('express');
 const bodyParser      = require('body-parser');
+const exphbs          = require('express-handlebars');
 const port = process.env.PORT || 5000;
 
 require('dotenv').config();
@@ -7,7 +8,13 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'))
+app.use(express.static('views'));
 
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+    res.render('home', {title: 'Purdue Timmy Global Health'});
+});
 
 var server = app.listen(port);

355326227d260eeaea19f170e539f84e10fee805
Cole Johnson <chj@purdue.edu>
2019-06-20 11:57:21 -0400

    Adds pages

--- index.js
+++ index.js
@@ -16,5 +16,17 @@ app.set('view engine', 'handlebars');
 app.get('/', function (req, res) {
     res.render('home', {title: 'Purdue Timmy Global Health'});
 });
+app.get('/about', function (req, res) {
+    res.render('about', {title: 'About | Purdue Timmy Global Health'});
+});
+app.get('/team', function (req, res) {
+    res.render('team', {title: 'Our Team | Purdue Timmy Global Health'});
+});
+app.get('/join', function (req, res) {
+    res.render('join', {title: 'Get Involved | Purdue Timmy Global Health'});
+});
+app.get('/timmytimes', function (req, res) {
+    res.render('timmytimes', {title: 'TimmyTimes | Purdue Timmy Global Health'});
+});
 
 var server = app.listen(port);
